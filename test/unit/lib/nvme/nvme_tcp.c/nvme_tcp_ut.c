ab47f23e33108f98b8e79dced132a26a2651d0bc
Alexey Marchuk <alexeymar@mellanox.com>
2020-07-06 14:04:42 +0300

    nvme_tcp: Connect qpair to poll group before sending icreq
    A preparation step for enabling zero copy in NVMEoF TCP initiator.
    When we initialize a new connection, we should complete handling of
    icreq when we receive both icresp and icreq send ack. To make it possible,
    we must add a qpair to poll group before we send icreq in order to be able
    to process async notifications.
    
    Change-Id: I4e084440c076385a163f772eb31baf1e3bbf6cbd
    Signed-off-by: Alexey Marchuk <alexeymar@mellanox.com>
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>

--- test/unit/lib/nvme/nvme_tcp.c/nvme_tcp_ut.c
+++ test/unit/lib/nvme/nvme_tcp.c/nvme_tcp_ut.c
@@ -51,6 +51,8 @@ DEFINE_STUB(spdk_sock_set_priority,
 DEFINE_STUB(spdk_nvme_poll_group_remove, int, (struct spdk_nvme_poll_group *group,
 		struct spdk_nvme_qpair *qpair), 0);
 
+DEFINE_STUB(nvme_poll_group_connect_qpair, int, (struct spdk_nvme_qpair *qpair), 0);
+
 static void
 test_nvme_tcp_pdu_set_data_buf(void)
 {
