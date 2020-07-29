336bced9cb3ff61496655870dcb78ead5da4fdec
Allen Zhu <allenz@mellanox.com>
2020-07-29 11:57:10 +0300

    nvme/perf: generate multiple-SGL payloads in perf tool A new option -O is added to indicate the IO unit size. Multiple IOVs are created according to the IO unit size. We are able to test multi SGL requests in NVMEoF RDMA with this patch.
    Signed-off-by: Allen Zhu <allenz@mellanox.com>
    Change-Id: I7624966b585bf0a9d2bbbb6263fa06fbcdb65820

--- examples/nvme/perf/perf.c
+++ examples/nvme/perf/perf.c
@@ -173,6 +173,10 @@ struct ns_worker_ctx {
 
 struct perf_task {
 	struct ns_worker_ctx	*ns_ctx;
+	struct iovec		*iovs; /** array of iovecs to transfer. */
+	int			iovcnt; /** Number of iovecs in iovs array. */
+	int			iovpos; /** Current iovec position. */
+	uint32_t		iov_offset; /** Offset in current iovec. */
 	struct iovec		iov;
 	struct iovec		md_iov;
 	uint64_t		submit_tsc;
@@ -203,6 +207,8 @@ struct ns_fn_table {
 
 	void	(*cleanup_ns_worker_ctx)(struct ns_worker_ctx *ns_ctx);
 };
+
+static uint32_t g_io_unit_size = UINT32_MAX;
 
 static int g_outstanding_commands;
 
@@ -261,6 +267,78 @@ static int g_file_optind; /* Index of first filename in argv */
 static inline void
 task_complete(struct perf_task *task);
 
+static void
+nvme_perf_reset_sgl(void *ref, uint32_t sgl_offset)
+{
+	struct iovec *iov;
+	struct perf_task *task = (struct perf_task *)ref;
+
+	task->iov_offset = sgl_offset;
+	for (task->iovpos = 0; task->iovpos < task->iovcnt; task->iovpos++) {
+		iov = &task->iovs[task->iovpos];
+		if (task->iov_offset < iov->iov_len) {
+			break;
+		}
+
+		task->iov_offset -= iov->iov_len;
+	}
+}
+
+static int
+nvme_perf_next_sge(void *ref, void **address, uint32_t *length)
+{
+	struct iovec *iov;
+	struct perf_task *task = (struct perf_task *)ref;
+
+	assert(task->iovpos < task->iovcnt);
+
+	iov = &task->iovs[task->iovpos];
+	assert(task->iov_offset <= iov->iov_len);
+
+	*address = iov->iov_base + task->iov_offset;
+	*length = iov->iov_len - task->iov_offset;
+	task->iovpos++;
+	task->iov_offset = 0;
+
+	return 0;
+}
+
+
+static int
+nvme_perf_allocate_iovs(struct perf_task *task, void *buf, uint32_t length)
+{
+	struct iovec *iov;
+	uint32_t offset = 0;
+	uint32_t iov_len = spdk_min(length, g_io_unit_size);
+
+	task->iovcnt = SPDK_CEIL_DIV(length, (uint64_t)g_io_unit_size);
+	if (task->iovcnt > 1) {
+		task->iovs = calloc(task->iovcnt, sizeof(struct iovec));
+		if (!task->iovs) {
+			return -1;
+		}
+	} else {
+		task->iovs = &task->iov;
+	}
+
+	for (task->iovpos = 0; task->iovpos < task->iovcnt; task->iovpos++) {
+		iov = &task->iovs[task->iovpos];
+		iov->iov_base = buf + offset;
+		iov->iov_len = iov_len;
+		offset += iov->iov_len;
+	}
+
+	/* The length of the last iov may less than g_io_unit_size */
+	if (offset > length) {
+		iov = &task->iovs[task->iovcnt - 1];
+		iov->iov_len -= offset - length;
+	}
+
+	task->iovpos = 0;
+
+	return 0;
+}
+
 #ifdef SPDK_CONFIG_URING
 
 static void
@@ -273,6 +351,8 @@ uring_setup_payload(struct perf_task *task, uint8_t pattern)
 		exit(1);
 	}
 	memset(task->iov.iov_base, pattern, task->iov.iov_len);
+
+	task->iovs = &task->iov;
 }
 
 static int
@@ -386,6 +466,8 @@ aio_setup_payload(struct perf_task *task, uint8_t pattern)
 		exit(1);
 	}
 	memset(task->iov.iov_base, pattern, task->iov.iov_len);
+
+	task->iovs = &task->iov;
 }
 
 static int
@@ -583,18 +665,26 @@ static void
 nvme_setup_payload(struct perf_task *task, uint8_t pattern)
 {
 	uint32_t max_io_size_bytes, max_io_md_size;
+	void *buf;
+	int rc;
 
 	/* maximum extended lba format size from all active namespace,
 	 * it's same with g_io_size_bytes for namespace without metadata.
 	 */
 	max_io_size_bytes = g_io_size_bytes + g_max_io_md_size * g_max_io_size_blocks;
-	task->iov.iov_base = spdk_dma_zmalloc(max_io_size_bytes, g_io_align, NULL);
-	task->iov.iov_len = max_io_size_bytes;
-	if (task->iov.iov_base == NULL) {
+	buf = spdk_dma_zmalloc(max_io_size_bytes, g_io_align, NULL);
+	if (buf == NULL) {
 		fprintf(stderr, "task->buf spdk_dma_zmalloc failed\n");
 		exit(1);
 	}
-	memset(task->iov.iov_base, pattern, task->iov.iov_len);
+	memset(buf, pattern, max_io_size_bytes);
+
+	rc = nvme_perf_allocate_iovs(task, buf, max_io_size_bytes);
+	if (rc < 0) {
+		fprintf(stderr, "perf task iov count exceeds MAX_IOVS\n");
+		spdk_dma_free(buf);
+		exit(1);
+	}
 
 	max_io_md_size = g_max_io_md_size * g_max_io_size_blocks;
 	if (max_io_md_size != 0) {
@@ -602,7 +692,10 @@ nvme_setup_payload(struct perf_task *task, uint8_t pattern)
 		task->md_iov.iov_len = max_io_md_size;
 		if (task->md_iov.iov_base == NULL) {
 			fprintf(stderr, "task->md_buf spdk_dma_zmalloc failed\n");
-			spdk_dma_free(task->iov.iov_base);
+			spdk_dma_free(task->iovs[0].iov_base);
+			if (task->iovcnt > 1) {
+				free(task->iovs);
+			}
 			exit(1);
 		}
 	}
@@ -650,23 +743,32 @@ nvme_submit_io(struct perf_task *task, struct ns_worker_ctx *ns_ctx,
 	}
 
 	if (task->is_read) {
-		return spdk_nvme_ns_cmd_read_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair[qp_num],
-						     task->iov.iov_base, task->md_iov.iov_base,
-						     lba,
-						     entry->io_size_blocks, io_complete,
-						     task, entry->io_flags,
-						     task->dif_ctx.apptag_mask, task->dif_ctx.app_tag);
+		if (task->iovcnt == 1) {
+			return spdk_nvme_ns_cmd_read_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair[qp_num],
+							     task->iovs[0].iov_base, task->md_iov.iov_base,
+							     lba,
+							     entry->io_size_blocks, io_complete,
+							     task, entry->io_flags,
+							     task->dif_ctx.apptag_mask, task->dif_ctx.app_tag);
+		} else {
+			return spdk_nvme_ns_cmd_readv_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair[qp_num],
+							      lba, entry->io_size_blocks,
+							      io_complete, task, entry->io_flags,
+							      nvme_perf_reset_sgl, nvme_perf_next_sge,
+							      task->md_iov.iov_base,
+							      task->dif_ctx.apptag_mask, task->dif_ctx.app_tag);
+		}
 	} else {
 		switch (mode) {
 		case DIF_MODE_DIF:
-			rc = spdk_dif_generate(&task->iov, 1, entry->io_size_blocks, &task->dif_ctx);
+			rc = spdk_dif_generate(task->iovs, task->iovcnt, entry->io_size_blocks, &task->dif_ctx);
 			if (rc != 0) {
 				fprintf(stderr, "Generation of DIF failed\n");
 				return rc;
 			}
 			break;
 		case DIF_MODE_DIX:
-			rc = spdk_dix_generate(&task->iov, 1, &task->md_iov, entry->io_size_blocks,
+			rc = spdk_dix_generate(task->iovs, task->iovcnt, &task->md_iov, entry->io_size_blocks,
 					       &task->dif_ctx);
 			if (rc != 0) {
 				fprintf(stderr, "Generation of DIX failed\n");
@@ -677,12 +779,21 @@ nvme_submit_io(struct perf_task *task, struct ns_worker_ctx *ns_ctx,
 			break;
 		}
 
-		return spdk_nvme_ns_cmd_write_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair[qp_num],
-						      task->iov.iov_base, task->md_iov.iov_base,
-						      lba,
-						      entry->io_size_blocks, io_complete,
-						      task, entry->io_flags,
-						      task->dif_ctx.apptag_mask, task->dif_ctx.app_tag);
+		if (task->iovcnt == 1) {
+			return spdk_nvme_ns_cmd_write_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair[qp_num],
+							      task->iovs[0].iov_base, task->md_iov.iov_base,
+							      lba,
+							      entry->io_size_blocks, io_complete,
+							      task, entry->io_flags,
+							      task->dif_ctx.apptag_mask, task->dif_ctx.app_tag);
+		} else {
+			return spdk_nvme_ns_cmd_writev_with_md(entry->u.nvme.ns, ns_ctx->u.nvme.qpair[qp_num],
+							       lba, entry->io_size_blocks,
+							       io_complete, task, entry->io_flags,
+							       nvme_perf_reset_sgl, nvme_perf_next_sge,
+							       task->md_iov.iov_base,
+							       task->dif_ctx.apptag_mask, task->dif_ctx.app_tag);
+		}
 	}
 }
 
@@ -715,14 +826,14 @@ nvme_verify_io(struct perf_task *task, struct ns_entry *entry)
 	}
 
 	if (entry->md_interleave) {
-		rc = spdk_dif_verify(&task->iov, 1, entry->io_size_blocks, &task->dif_ctx,
+		rc = spdk_dif_verify(task->iovs, task->iovcnt, entry->io_size_blocks, &task->dif_ctx,
 				     &err_blk);
 		if (rc != 0) {
 			fprintf(stderr, "DIF error detected. type=%d, offset=%" PRIu32 "\n",
 				err_blk.err_type, err_blk.err_offset);
 		}
 	} else {
-		rc = spdk_dix_verify(&task->iov, 1, &task->md_iov, entry->io_size_blocks,
+		rc = spdk_dix_verify(task->iovs, task->iovcnt, &task->md_iov, entry->io_size_blocks,
 				     &task->dif_ctx, &err_blk);
 		if (rc != 0) {
 			fprintf(stderr, "DIX error detected. type=%d, offset=%" PRIu32 "\n",
@@ -1129,7 +1240,10 @@ task_complete(struct perf_task *task)
 	 * the one just completed.
 	 */
 	if (spdk_unlikely(ns_ctx->is_draining)) {
-		spdk_dma_free(task->iov.iov_base);
+		spdk_dma_free(task->iovs[0].iov_base);
+		if (task->iovcnt > 1) {
+			free(task->iovs);
+		}
 		spdk_dma_free(task->md_iov.iov_base);
 		free(task);
 	} else {
@@ -1311,6 +1425,7 @@ static void usage(char *program_name)
 	printf("\n");
 	printf("\t[-q io depth]\n");
 	printf("\t[-o io size in bytes]\n");
+	printf("\t[-O io unit size in bytes]\n");
 	printf("\t[-P number of io queues per namespace. default: 1]\n");
 	printf("\t[-U number of unused io queues per controller. default: 0]\n");
 	printf("\t[-w io pattern type, must be one of\n");
@@ -1763,12 +1878,13 @@ parse_args(int argc, char **argv)
 	long int val;
 	int rc;
 
-	while ((op = getopt(argc, argv, "c:e:i:lo:q:r:k:s:t:w:C:DGHILM:NP:RT:U:V")) != -1) {
+	while ((op = getopt(argc, argv, "c:e:i:lo:O:q:r:k:s:t:w:C:DGHILM:NP:RT:U:V")) != -1) {
 		switch (op) {
 		case 'i':
 		case 'C':
 		case 'P':
 		case 'o':
+		case 'O':
 		case 'q':
 		case 'k':
 		case 's':
@@ -1792,6 +1908,9 @@ parse_args(int argc, char **argv)
 				break;
 			case 'o':
 				g_io_size_bytes = val;
+				break;
+			case 'O':
+				g_io_unit_size = val;
 				break;
 			case 'q':
 				g_queue_depth = val;
@@ -1907,6 +2026,10 @@ parse_args(int argc, char **argv)
 	if (!g_io_size_bytes) {
 		fprintf(stderr, "missing -o (block size) operand\n");
 		usage(argv[0]);
+		return 1;
+	}
+	if (!g_io_unit_size) {
+		fprintf(stderr, "io unit size can not be 0\n");
 		return 1;
 	}
 	if (!g_workload_type) {
